8aa3d3f9c92ccaf53be74605a4f02f18e7014d3d
yoshi-hanada <89883599+yoshi-hanada@users.noreply.github.com>
2021-09-13 10:27:19 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'BAGY',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'TUISEKI',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'LOVE',
           // q2: 'かかか',
           // q3: 'ききき',
         }
