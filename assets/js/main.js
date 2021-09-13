acbae43953b36e2d910ba86c8b8752171aa49886
yoshi-hanada <89883599+yoshi-hanada@users.noreply.github.com>
2021-09-13 21:57:20 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,7 +111,7 @@ app.component('answer-input', {
         <input type="text" v-model="inputAnswer" placeholder="ここに答えを入力しよう">
       </div>
       <p v-if="message === ngMessage" class="err-message">{{ message }}</p>
-      <button v-on:click="judgement(inputAnswer)">送信</button>
+      <button v-on:click="judgement(inputAnswer)" class="send-button">送信</button>
       <p v-if="message === okMessage" class="err-message">{{ message }}</p>
     </div>`,
   methods: {
